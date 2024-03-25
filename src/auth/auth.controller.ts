498acd6f47cfe65716e86fc246c3534aa0d5b81d
Cao Xuan Son <caoxuanson1706@gmail.com>
2024-03-25 15:25:36 +0700

    Implement register user endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,23 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { Routes, Services } from 'src/utils/constants';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/CreateUserDto';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(@Inject(Services.AUTH) private authService: AuthService) {}
+
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+
+  @Post('login')
+  login() {}
+
+  @Get('status')
+  status() {}
+
+  @Post('logout')
+  logout() {}
 }
